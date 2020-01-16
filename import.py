1aa6d085f65af372da6024202fa8f7d724479050
tomdevalk1997 <tomdevalk@hotmail.nl>
2020-01-16 14:28:18 +0100

    DATABASE_URL in import.py - not sure if correct

--- import.py
+++ import.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("postgres://iutmwfhcnvzjeh:723a5f85dd12d10d4f2c44a58f7781d856595212cca2d1573a176598f4cec44d@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/dej617ep6sqm62"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():

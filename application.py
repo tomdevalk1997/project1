730cb3dbcae613cc98ccfe610e14db17c57aebd6
tomdevalk1997 <tomdevalk@hotmail.nl>
2020-01-17 15:53:53 +0100

    Layout.html en render eerste pagina

--- application.py
+++ application.py
@@ -1,26 +1,25 @@
 import os
 
 from flask import Flask, session
-from flask_session import Session
+#from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import csv
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("postgres://iutmwfhcnvzjeh:723a5f85dd12d10d4f2c44a58f7781d856595212cca2d1573a176598f4cec44d@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/dej617ep6sqm62"))
+#db = scoped_session(sessionmaker(bind=engine))
 
+exec('import.py')
+print("done")
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")

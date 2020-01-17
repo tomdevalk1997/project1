01c29e80c0ba71ec221fe5037f18a21d2fd1e56b
tomdevalk1997 <tomdevalk@hotmail.nl>
2020-01-17 15:59:23 +0100

    Import flask.render_template

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, session
-import flask.render_template
+from flask import render_template
 #from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
